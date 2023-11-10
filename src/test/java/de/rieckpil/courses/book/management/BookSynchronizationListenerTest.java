edeb91fafecc2e6443f58bf941742a44e3839508
Trinn Tamás <trinn.tamas@webstar.hu>
2023-11-10 13:39:55 +0100

    [test] BookSynchronizationListener

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class BookSynchronizationListenerTest {
+class  BookSynchronizationListenerTest {
 
   private final static String VALID_ISBN = "1234567891234";
 
@@ -28,18 +28,66 @@ class BookSynchronizationListenerTest {
 
   @Test
   void shouldRejectBookWhenIsbnIsMalformed() {
+    BookSynchronization bookSynchronization = new BookSynchronization("42");
+
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verifyNoInteractions(openLibraryApiClient, bookRepository);
   }
 
   @Test
   void shouldNotOverrideWhenBookAlreadyExists() {
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(new Book());
+
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verifyNoInteractions(openLibraryApiClient);
+
+    verify(bookRepository, times(0)).save(ArgumentMatchers.any());
   }
 
   @Test
   void shouldThrowExceptionWhenProcessingFails() {
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN))
+      .thenThrow(new RuntimeException("Network timeout"));
+
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+
+    assertThrows(RuntimeException.class, () -> cut.consumeBookUpdates(bookSynchronization));
   }
 
   @Test
   void shouldStoreBookWhenNewAndCorrectIsbn() {
+    // ARANGE
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+
+    Book requestedBook = new Book();
+    requestedBook.setTitle("Java book");
+    requestedBook.setIsbn(VALID_ISBN);
+
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN))
+      .thenReturn(requestedBook);
+    when(bookRepository.save(ArgumentMatchers.any())).then(invocationOnMock -> {
+      Book methodArgument = invocationOnMock.getArgument(0);
+      methodArgument.setId(1L);
+
+      return methodArgument;
+    });
+
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+
+    // ACT
+    cut.consumeBookUpdates(bookSynchronization);
+
+    // ASSERT
+    verify(bookRepository).save(bookArgumentCaptor.capture());
+
+    Book methodArgument = bookArgumentCaptor.getValue();
+    assertEquals("Java book", methodArgument.getTitle());
+    assertEquals(VALID_ISBN, methodArgument.getIsbn());
   }
 
 }
