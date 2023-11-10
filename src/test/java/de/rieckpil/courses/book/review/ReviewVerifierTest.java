98e0e54828ea58283e217dbd5ba1bc55c0a6273a
Trinn Tamás <trinn.tamas@webstar.hu>
2023-11-10 09:12:20 +0100

    [test] ReviewVerifier: invalid review containing lorem ipsum

--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -1,8 +1,5 @@
 package de.rieckpil.courses.book.review;
 
-import org.assertj.core.api.Assertions;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
@@ -11,11 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import java.util.List;
-
 import static de.rieckpil.courses.book.review.RandomReviewParameterResolverExtension.RandomReview;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(RandomReviewParameterResolverExtension.class)
 class ReviewVerifierTest {
@@ -39,6 +33,15 @@ class ReviewVerifierTest {
   @Test
   @DisplayName("Should fail when review contains 'lorem ipsum'")
   void testLoremIpsum() {
+    String review = "Lorem ipsum is simply dummy text of the printing and typesetting industry. " +
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer " +
+      "took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, " +
+      "but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the " +
+      "1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop " +
+      "publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect lorem ipsum");
   }
 
   @ParameterizedTest
