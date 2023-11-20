161417f0255c73b06ebe4fa4b32ea013fb82fb2b
Trinn Tamás <trinn.tamas@webstar.hu>
2023-11-20 14:17:45 +0100

    [test] BookControllerTest

--- src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
@@ -32,14 +31,63 @@ class BookControllerTest {
 
   @Test
   void shouldGetEmptyArrayWhenNoBooksExists() throws Exception {
+    mockMvc.perform(get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.size()", is(0)))
+      .andDo(print());
   }
 
   @Test
   void shouldNotReturnXML() throws Exception {
+    mockMvc.perform(get("/api/books")
+      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML)
+    )
+      .andExpect(status().isNotAcceptable());
   }
 
   @Test
   void shouldGetBooksWhenServiceReturnsBooks() throws Exception {
+    Book bookOne = createBook(1L, "42", "Java 14", "Mike", "Good book",
+      "Software Engineering", 200L, "Oracle", "ftp://localhost:42");
+
+    Book bookTwo = createBook(2L, "84", "Java 15", "Duke", "Good book",
+      "Software Engineering", 200L, "Oracle", "ftp://localhost:84");
+
+    when(bookManagementService.getAllBooks()).thenReturn(List.of(bookOne, bookTwo));
+
+    mockMvc.perform(get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.size()", is(2)))
+      .andExpect(jsonPath("$[0].id").doesNotExist())
+      .andExpect(jsonPath("$[0].isbn", is("42")))
+      .andExpect(jsonPath("$[0].title", is("Java 14")))
+      .andExpect(jsonPath("$[0].author", is("Mike")))
+      .andExpect(jsonPath("$[0].description", is("Good book")))
+      .andExpect(jsonPath("$[0].genre", is("Software Engineering")))
+      .andExpect(jsonPath("$[0].pages", is(200)))
+      .andExpect(jsonPath("$[0].publisher", is("Oracle")))
+      .andExpect(jsonPath("$[0].thumbnailUrl", is("ftp://localhost:42")))
+      .andExpect(jsonPath("$[1].id").doesNotExist())
+      .andExpect(jsonPath("$[1].isbn", is("84")))
+      .andExpect(jsonPath("$[1].title", is("Java 15")))
+      .andExpect(jsonPath("$[1].author", is("Duke")))
+      .andExpect(jsonPath("$[1].description", is("Good book")))
+      .andExpect(jsonPath("$[1].genre", is("Software Engineering")))
+      .andExpect(jsonPath("$[1].pages", is(200)))
+      .andExpect(jsonPath("$[1].publisher", is("Oracle")))
+      .andExpect(jsonPath("$[1].thumbnailUrl", is("ftp://localhost:84")))
+      ;
+
+    // minden property tesztelésre másik módszer a JSONAssert
+    // What you would need to do is get the HTTP response body from the test as a String and then use
+    // JSONAssert.assertEquals(expected, data);. A simple String comparison won’t do the trick as the order of
+    // the JSOn attributes might differ.
   }
 
   private Book createBook(Long id, String isbn, String title, String author, String description, String genre, Long pages, String publisher, String thumbnailUrl) {
