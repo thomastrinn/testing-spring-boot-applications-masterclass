68ea30fb76168942c1bb5779fd9fef33a558617b
Trinn Tamás <trinn.tamas@webstar.hu>
2023-11-22 14:58:31 +0100

    [test] ReviewControllerIT

--- src/test/java/de/rieckpil/courses/book/review/ReviewControllerIT.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewControllerIT.java
@@ -12,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ReviewControllerIT extends AbstractIntegrationTest {
 
   private static final String ISBN = "9780596004651";
@@ -22,11 +24,119 @@ class ReviewControllerIT extends AbstractIntegrationTest {
   @Autowired
   private BookRepository bookRepository;
 
+  @BeforeEach
+  void setup() {
+    Book book = new Book();
+    book.setPublisher("Duke Inc.");
+    book.setIsbn(ISBN);
+    book.setPages(42L);
+    book.setTitle("Joyful testing with Spring Boot");
+    book.setDescription("Writing unit and integration tests for Spring Boot applications");
+    book.setAuthor("rieckpil");
+    book.setThumbnailUrl(
+      "https://rieckpil.de/wp-content/uploads/2020/08/tsbam_introduction_thumbnail-585x329.png.webp");
+    book.setGenre("Software Development");
+
+    bookRepository.save(book);
+  }
+
   @Test
   void shouldReturnCreatedReviewWhenBookExistsAndReviewHasGoodQuality() throws JOSEException {
+    String reviewPayload = """
+    {
+      "reviewTitle" : "Great book with lots of tips & tricks",
+      "reviewContent" : "I can really recommend reading this book. It includes up-to-date library versions and real-world examples",
+      "rating": 4
+    }
+    """;
+    String validJWT = getSignedJWT();
+
+    HttpHeaders responseHeaders = webTestClient
+      .post()
+      .uri("/api/books/{isbn}/reviews", ISBN)
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + validJWT)
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue(reviewPayload)
+      .exchange()
+      .expectStatus().isCreated()
+      .returnResult(ResponseEntity.class)
+      .getResponseHeaders();
+
+    assertNotNull(responseHeaders);
+    assertNotNull(responseHeaders.getLocation());
+
+    webTestClient
+      .get()
+      .uri(responseHeaders.getLocation())
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + validJWT)
+      .exchange()
+      .expectStatus().isOk()
+      .expectBody()
+      .jsonPath("$.reviewTitle").isEqualTo("Great book with lots of tips & tricks")
+      .jsonPath("$.rating").isEqualTo(4)
+      .jsonPath("$.bookIsbn").isEqualTo(ISBN);
   }
 
   @Test
   void shouldReturnReviewStatisticWhenMultipleReviewsForBookFromDifferentUsersExist() throws JOSEException {
+    // ARANGE: create 3 reviews
+    webTestClient
+      .post()
+      .uri("/api/books/{isbn}/reviews", ISBN)
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + getSignedJWT("mike", "mike@spring.io"))
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue("""
+    {
+      "reviewTitle" : "Great book with lots of tips & tricks",
+      "reviewContent" : "I can really recommend reading this book. It includes up-to-date library versions and real-world examples",
+      "rating": 5
+    }
+    """)
+      .exchange()
+      .expectStatus().isCreated();
+
+    webTestClient
+      .post()
+      .uri("/api/books/{isbn}/reviews", ISBN)
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + getSignedJWT("duke", "duke@spring.io"))
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue("""
+    {
+      "reviewTitle" : "Great book with lots of tips & tricks",
+      "reviewContent" : "I can really recommend reading this book. It includes up-to-date library versions and real-world examples",
+      "rating": 3
+    }
+    """)
+      .exchange()
+      .expectStatus().isCreated();
+
+    webTestClient
+      .post()
+      .uri("/api/books/{isbn}/reviews", ISBN)
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + getSignedJWT("mandy", "mandy@spring.io"))
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue("""
+    {
+      "reviewTitle" : "Great book with lots of tips & tricks",
+      "reviewContent" : "I can really recommend reading this book. It includes up-to-date library versions and real-world examples",
+      "rating": 4
+    }
+    """)
+      .exchange()
+      .expectStatus().isCreated();
+
+    // ACT: get review statistics and ASSERT
+    webTestClient
+      .get()
+      .uri("/api/books/reviews/statistics")
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + getSignedJWT())
+      .exchange()
+      .expectStatus().isOk()
+      .expectBody()
+      .jsonPath("$.size()").isEqualTo(1)
+      .jsonPath("$[0].isbn").isEqualTo(ISBN)
+      .jsonPath("$[0].ratings").isEqualTo(3)
+      .jsonPath("$[0].avg").isEqualTo(4.00);
+
   }
 }
